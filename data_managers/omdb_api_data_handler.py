defcb97af479e3f855b5af5107d0c6d52763f2f7
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-06-30 04:20:04 +0300

    finished inner logic

--- data_managers/omdb_api_data_handler.py
+++ data_managers/omdb_api_data_handler.py
@@ -1,24 +1,41 @@
 import requests
-
-API_KEY = "f994fda"
+import logging
 
 class MovieAPIHandler():
+    IMDB_PATH = "https://www.imdb.com/title/"
+
     def __init__(self):
         self._api_key = "f994fda"
         self._url = "https://www.omdbapi.com/"
+        self._logger = logging.getLogger(__name__)
+
     def get_movie_by_title(self, title: str) -> dict:
         """Search for movie by title. Returns dict with data of found movie"""
         params = {
-            'apikey': 'f994fda',
+            'apikey': self._api_key,
             't': title
         }
         try:
             response = requests.get(self._url, params=params)
             response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
 
-            return response.json()  # Parse the response as JSON
+            processed_movie_data = self._transform_movie_data(response.json())
+            return processed_movie_data
         except requests.exceptions.RequestException as e:
+            self._logger.error(f"An error occurred: {str(e)}")
             return None
 
+    def _transform_movie_data(self, api_data: dict) -> dict:
+        """Transform api data to match the data used in app"""
+        transformed_data = {
+            'id': api_data['imdbID'],
+            'name': api_data['Title'],
+            'director': api_data['Director'],
+            'year': api_data['Year'],
+            'rating': api_data['imdbRating'],
+            'imdb_link': MovieAPIHandler.IMDB_PATH + api_data['imdbID'],
+            'image_link': api_data['Poster']
+        }
+        return transformed_data
 
 
