f1090c592919b24579a8430d11ce3bb5787e8af5
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-07-01 06:05:09 +0300

    added error handling and logging for errors

--- app.py
+++ app.py
@@ -1,81 +1,122 @@
-from flask import Flask, request, render_template, redirect, url_for
+import logging
+from flask import Flask, request, render_template, redirect, url_for, abort
 from data_managers.json_data_manager import JSONDataManager
 from data_managers.omdb_api_data_handler import MovieAPIHandler
 
-import logging
-import os
 
+# Create or get the logger
+logger = logging.getLogger(__name__)  # __name__ resolves to the name of the module, class, or function that called this logging setup
 
-# if not os.path.exists('logs'):
-#     os.makedirs('logs')
-#
-# logging.basicConfig(filename='logs/app.log', level=logging.INFO)
+# set log level
+logger.setLevel(logging.ERROR)
+
+handler = logging.FileHandler('logs/app.log')  # set the log handler
+handler.setLevel(logging.ERROR)  # set the handler level
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')  # create a logging format
+handler.setFormatter(formatter)  # set the logging format for the handler
+
+logger.addHandler(handler)
 
 app = Flask(__name__)
 
 
-#------- ROUTES -------------------------------------------------------------
-
+# ------- ROUTES -------------------------------------------------------------
 
 @app.route('/')
 def list_users():
-    users = data_manager.get_all_users()
-    return render_template('users.html', users=users)
+    """Main page endpoint, return rendered page with users"""
+    try:
+        users = data_manager.get_all_users()
+        return render_template('users.html', users=users)
+    except Exception as e:
+        abort(404)
 
 @app.route('/user_movies')
 def user_movies():
-    user_id = request.args.get('user_id', type=int)
-    user = data_manager.get_user_by_id(user_id)
+    try:
+        user_id = int(request.args.get('user_id'))
+        user = data_manager.get_user_by_id(user_id)
+        return render_template('user_movies.html', user=user)
+    except Exception as e:
+        logger.exception("Exception occurred")
+        abort(404)
 
-    return render_template('user_movies.html', user=user)
-
-
-@app.route('/add_user')  # should be post
+@app.route('/add_user', methods=["POST"])
 def add_user():
-    user_name = request.args.get('user_name')
-    if user_name:
-        data_manager.add_user(user_name)
+    try:
+        user_name = request.form.get('user_name')
+        if user_name:
+            data_manager.add_user(user_name)
 
-    return redirect(url_for('list_users'))
-    #  This route will present a form that enables the addition of a new user to our MovieWeb App.
-
+        return redirect(url_for('list_users'))
+    except Exception as e:
+        logger.exception("Exception occurred")
+        abort(404)
 
 @app.route('/add_movie', methods=['POST'])
 def add_movie():
-    movie_name_to_search: str = request.form.get('search_name')
-    user_id = int(request.form.get('user_id'))
-    if movie_name_to_search:
-        movie_to_add = movies_api_handler.get_movie_by_title(movie_name_to_search)
-        if movie_to_add:
-            data_manager.add_movie_to_user(user_id, movie_to_add)
-    user = data_manager.get_user_by_id(user_id)
+    try:
+        movie_name_to_search: str = request.form.get('search_name')
+        user_id = int(request.form.get('user_id'))
+        if movie_name_to_search:
+            movie_to_add = movies_api_handler.get_movie_by_title(
+                movie_name_to_search)
+            if movie_to_add:
+                data_manager.add_movie_to_user(user_id, movie_to_add)
+        user = data_manager.get_user_by_id(user_id)
 
-    return redirect(url_for('user_movies', user_id=user_id))
+        return redirect(url_for('user_movies', user_id=user_id))
+    except Exception as e:
+        logger.exception("Exception occurred")
+        abort(404)
 
 
 @app.route("/delete_user/<int:user_id>", methods=["POST"])
 def delete_user(user_id: int):
-    data_manager.delete_user(user_id)
+    try:
+        data_manager.delete_user(user_id)
+        return redirect(url_for('list_users'))
+    except Exception as e:
+        logger.exception("Exception occurred")
+        abort(404)
 
-    return redirect(url_for('user_movies'))
+@app.route('/users/<int:user_id>/edit_movie/<movie_id>', methods=['GET', 'POST'])
+def update_movie(user_id, movie_id):
+    try:
+        if request.method == 'GET':
+            movie_for_update = data_manager.get_user_movie(user_id, movie_id)
+            return render_template('edit_movie.html', movie=movie_for_update,
+                                   user_id=user_id, movie_id=movie_id)
 
+        if request.method == 'POST':
+            movie_data_to_update = {}
+            for key, value in request.form.items():
+                movie_data_to_update[key] = value
+            data_manager.update_movie_of_user(user_id, movie_id,
+                                              movie_data_to_update)
 
-
-@app.route('/users/<user_id>/edit_movie/<movie_id>', methods=['POST'])
-def edit_movie(user_id, movie_id):
-    pass
-    # This route will show a form allowing the modification of a specific
-    # movie in a user’s favorite movie list.
+        return redirect(url_for('user_movies', user_id=user_id))
+    except Exception as e:
+        logger.exception("Exception occurred")
+        abort(404)
 
 
 @app.route('/users/<int:user_id>/delete_movie/<movie_id>', methods=["POST"])
 def delete_movie(user_id, movie_id):
-    data_manager.delete_movie_of_user(user_id, movie_id)
-    return redirect(url_for('user_movies', user_id=user_id))
+    try:
+        data_manager.delete_movie_of_user(user_id, movie_id)
+        return redirect(url_for('user_movies', user_id=user_id))
+    except Exception as e:
+        logger.exception("Exception occurred")
+        abort(404)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
-    # logging.basicConfig(level=logging.INFO)
     data_manager = JSONDataManager('movies_test_2')
     movies_api_handler = MovieAPIHandler()
     app.run(port=5000, debug=True)
