cc665a608492bcfac16509088d992ec0957cbac2
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-07-05 20:00:42 +0300

    added raise ValueError to delete_movie_of_user

--- tests/test_json_data_manager.py
+++ tests/test_json_data_manager.py
@@ -4,41 +4,75 @@ import pytest
 
 FILE_NAME = "test_data_manager"
 DATA_FOLDER_NAME = 'data'
+FILE_PATH = os.path.join(DATA_FOLDER_NAME, FILE_NAME + '.json')
 
+@pytest.fixture
+def json_manager():
+    manager = JSONDataManager(FILE_NAME)
+    yield manager
+    # Teardown
+    if os.path.exists(FILE_PATH):
+        os.remove(FILE_PATH)
 
-def test_create_new_manager():
-    # creating new instance for JSONDataManager. checking if file created and deleting the file
-    json_manager = JSONDataManager(FILE_NAME)
-    file_path = os.path.join(DATA_FOLDER_NAME, FILE_NAME + '.json')
-    assert os.path.exists(file_path)
-    if os.path.exists(file_path):
-        os.remove(file_path)
+def test_file_created_after_manager_initialization(json_manager):
+    assert os.path.exists(FILE_PATH)
 
-def test_add_user():
-    json_manager = JSONDataManager(FILE_NAME)
-    file_path = os.path.join(json_manager.file_name)
-
-    # test adding new user
+def test_add_valid_user(json_manager):
     json_manager.add_user("bob")
     users = json_manager.get_all_users()
     assert users[0]['name'] == 'bob'
     assert users[0]['id'] == 1
 
-    # test passing not string variable will raise an error
+def test_add_user_with_integer_name(json_manager):
     with pytest.raises(TypeError):
         json_manager.add_user(2)
 
-    # passing empty string raise an error
+def test_add_user_with_empty_string(json_manager):
     with pytest.raises(ValueError):
         json_manager.add_user('')
 
-    # passing name that already exists
+def test_add_user_with_existing_name(json_manager):
+    json_manager.add_user("bob")
     with pytest.raises(ValueError):
         json_manager.add_user('bob')
 
+def test_movie_added_to_user(json_manager):
+    json_manager.add_user("bob")
+    movie_to_add = {"id": "tt0091757", "name": "Pirates",
+                    "director": "Roman Polanski",
+                    "year": "1986", "rating": "6.0",
+                    "imdb_link": "https://www.test.com",
+                    "image_link": "https:test-link.jpg"
+                    }
+    json_manager.add_movie_to_user(1, movie_to_add)
+    added_movie = json_manager.get_user_movie(1, movie_to_add['id'])
+    assert added_movie == movie_to_add
 
-    if os.path.exists(file_path):
-        os.remove(file_path)
+def test_delete_movie_of_user(json_manager):
+    json_manager.add_user("bob")
+    movie_to_add = {"id": "tt0091757", "name": "Pirates",
+                    "director": "Roman Polanski",
+                    "year": "1986", "rating": "6.0",
+                    "imdb_link": "https://www.test.com",
+                    "image_link": "https:test-link.jpg"
+                    }
+    # adding movie to user
+    json_manager.add_movie_to_user(1, movie_to_add)
 
+    # deleting movie from user and testing that it deleted
+    json_manager.delete_movie_of_user(1, movie_to_add['id'])
+    users = json_manager.get_all_users()
+    assert len(users[0]['movies']) == 0, "movie deleted"
+
+
+def test_delete_movie_of_unexisted_user(json_manager):
+    error_message = 'User with id 1 does not exist'
+    with pytest.raises(ValueError, match=error_message):
+        json_manager.delete_movie_of_user(1, 'fdfd33refd')
+
+def test_delete_movie_that_does_not_exist(json_manager):
+    error_message = 'User with id 1 does not exist'
+    with pytest.raises(ValueError, match=error_message):
+        json_manager.delete_movie_of_user(1, 'fdfd33refd')
 
 pytest.main()
